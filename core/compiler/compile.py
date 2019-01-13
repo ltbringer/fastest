b305c476104052a780164f77f9dd80b72cfa4cd1
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-01-14 05:22:40 +0530

    add: naive test case instead of type checks

--- core/compiler/compile.py
+++ core/compiler/compile.py
@@ -3,8 +3,10 @@ from core.bodies import f
 
 
 def build(function_objects, src_file_path, base_path):
-    test_file_path = src_file_path.replace('.py', '__test.py')
+    test_file_name = src_file_path.split('/')[-1].replace('.py', '__test.py')
     deps_import = src_file_path.replace(base_path + '/', '').replace('/', '.').replace('.py', '')
+    root_module_name = deps_import.split('.')[-1]
+    test_file_path = os.path.join(base_path, 'test', test_file_name)
 
     with open(test_file_path, 'w+') as fp:
         fp.write('import unittest\n\n')
@@ -14,19 +16,9 @@ def build(function_objects, src_file_path, base_path):
         for function_object in function_objects:
             fp.write(
                 """\n
-class Test_{}(unittest.TestCase):
+class Test_{}_{}(unittest.TestCase):
            
-                """.format(function_object['name'])
+                """.format(root_module_name, function_object['name'])
             )
-            for arg in function_object['args']:
-                fp.write(
-                    f.create_var_type_test_case(
-                        function_object, arg, function_object['str']
-                    ))
-            fp.write(
-                f.create_return_type_test_case(
-                    function_object,
-                    function_object['returns'],
-                    function_object['str']
-                ))
-
+            for test in function_object['tests']:
+                fp.write(f.create_naive_test_case(function_object, test))
