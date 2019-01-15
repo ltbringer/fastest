e483200d04772a4131d7a60430f9484717eb76fb
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-01-16 03:14:14 +0530

    add: empowered docstrings, can create variables and include source modules and libs

--- fastest/code_assets/naive_case_detector.py
+++ fastest/code_assets/naive_case_detector.py
@@ -1,39 +1,74 @@
 import re
+from fastest.constants import KEYS, PATTERNS
 
 
 FUNCTION_CALL = 0
 OUTPUT = 1
 
 
-def get_naive_case(statements):
-    """
-    example: get_naive_case("example: fn_do_work() -> 8 #") -> [{
-        "from": "fn_do_work()",
-        "expect": 8
-    }]
-    #
+def stack_imports(import_statements):
+    return [
+        import_statement.strip() + '\n'
+        for import_statement in import_statements
+        if len(import_statement) > 0
+    ]
 
-    :param statements:
-    :return:
-    """
-    function_call_pattern = r'example: [\s\S]+?(?=->)'
-    total_pattern = r'example: [\s\S]+?(?=#)'
-    function_call_matches = re.findall(function_call_pattern, statements, re.M)
-    total_matches = re.findall(total_pattern, statements, re.M)
 
-    test_cases = []
+def stack_variables(variable_string):
+    if type(variable_string) is not str:
+        return ''
+    variables = [
+        variable.strip()
+        for variable in variable_string.split(PATTERNS.VAR_DEC)
+    ]
+    return variables
 
-    for func_match, total_match in zip(function_call_matches, total_matches):
-        func_match = func_match.replace('example:', '').strip()
 
-        expectation = total_match\
-          .replace(func_match, '')\
-          .replace('example:', '')\
-          .replace('->', '')\
-          .strip()
+def get_imports_from_docstring(example_passage):
+    needed_imports = re.findall(PATTERNS.NEED_IMPORT, example_passage, re.M)
+    needed_imports = needed_imports if len(needed_imports) > 0 else None
+    if needed_imports is None:
+        return []
+    needed_imports = ''.join(needed_imports).replace(PATTERNS.IMPORT_DEC, '').split('\n')
+    return stack_imports(needed_imports)
 
-        test_cases.append({
-            'from': func_match,
-            'expect': expectation
+
+def get_variables_from_docstring(example_passage):
+    needed_variables = re.findall(r'@let[\s\S]+?(?=\d\))', example_passage)
+    needed_variables = needed_variables if len(needed_variables) > 0 else []
+    return stack_variables(''.join(needed_variables))
+
+
+def stack_examples(examples_strings):
+    example_stack = []
+    for example in examples_strings:
+        test_function, expectation = re.sub(PATTERNS.NUMBER_BULLET, '', example, 1).split(PATTERNS.TEST_SEP)
+
+        example_stack.append({
+            KEYS.FROM: test_function,
+            KEYS.EXPECT: expectation
         })
-    return test_cases
+    return example_stack
+
+
+def get_test_case_examples(example_passage):
+    examples_strings = re.findall(PATTERNS.TEST_CASE_EXAMPLE, example_passage, re.M)
+    examples_strings = examples_strings if len(examples_strings) > 0 else None
+    return stack_examples(examples_strings)
+
+
+def get_test_from_example_passage(statements):
+    example_passage = re.findall(PATTERNS.EXAMPLE_PASSAGE, statements, re.I)
+    example_passage = example_passage[0] if len(example_passage) > 0 else None
+    if example_passage is None:
+        return None
+    import_statements = get_imports_from_docstring(example_passage)
+    variables = get_variables_from_docstring(example_passage)
+    examples = get_test_case_examples(example_passage)
+    return None \
+        if examples is None \
+        else {
+            KEYS.IMPORTS: import_statements,
+            KEYS.VARIABLES: variables,
+            KEYS.EXAMPLES: examples
+        }
--- fastest/compiler/compile_tests.py
+++ fastest/compiler/compile_tests.py
@@ -1,20 +1,62 @@
 import os
 from fastest.bodies import f
+from fastest.constants import CONTENT, KEYS, SYS
+
+
+
+
+def add_imports_for_test_case(test, imports):
+    if test[KEYS.IMPORTS] is None:
+        return imports
+    for import_statement in test[KEYS.IMPORTS]:
+        imports.add(import_statement)
+    return imports
+
+def create_test_class(imports, contents, deps_import, function_object, root_module_name):
+    if len(function_object[KEYS.TESTS]) == 0:
+        return None
+
+    imports.add(CONTENT.IMPORT_UNITTEST)
+    imports.add(CONTENT.DEPS_IMPORT_TEMPLATE.format(deps_import, function_object[KEYS.NAME]))
+    contents.append(CONTENT.CLASS_CREATE_TEMPLATE.format(root_module_name, function_object[KEYS.NAME]))
+    return imports, contents
+
+
+def create_test_case_content(function_object, imports, contents):
+    for example in function_object[KEYS.TESTS][KEYS.EXAMPLES]:
+        contents.append(f.create_naive_test_case(function_object, example))
+    imports = add_imports_for_test_case(function_object[KEYS.TESTS], imports)
+    return imports, contents
+
+
+def create_test_case(function_objects, deps_import, root_module_name):
+    imports = set()
+    contents = []
+
+    for function_object in function_objects:
+        if function_object is None:
+            continue
+
+        if function_object[KEYS.TESTS] is None:
+            continue
+
+        imports, contents = create_test_class(imports, contents, deps_import, function_object, root_module_name)
+        imports, contents = create_test_case_content(function_object, imports, contents)
+    return imports, contents
+
+
+def write_tests_to_file(fp, imports, contents):
+    return fp.write(''.join(sorted(list(imports)) + contents))
 
 
 def build(function_objects, src_file_path, base_path):
-    test_file_name = src_file_path.split('/')[-1].replace('.py', '__test.py')
-    deps_import = src_file_path.replace(base_path + '/', '').replace('/', '.').replace('.py', '')
+    last_file = -1
+    test_file_name = src_file_path.split(SYS.SLASH)[last_file].replace('.py', SYS.TEST_FILE_ENDING)
+    deps_import = src_file_path.replace(base_path + SYS.SLASH, '').replace(SYS.SLASH, '.').replace('.py', '')
     root_module_name = deps_import.split('.')[-1]
-    test_file_path = os.path.join(base_path, 'test', test_file_name)
+    test_file_path = os.path.join(base_path, KEYS.TEST, test_file_name)
+
 
     with open(test_file_path, 'w+') as fp:
-        fp.write('import unittest\n\n')
-        for function_object in function_objects:
-            fp.write('from {} import {}\n'.format(deps_import, function_object['name']))
-
-        for function_object in function_objects:
-            fp.write("""\n
-class Test_{}_{}(unittest.TestCase):""".format(root_module_name, function_object['name']))
-            for test in function_object['tests']:
-                fp.write(f.create_naive_test_case(function_object, test))
+        imports, contents = create_test_case(function_objects, deps_import, root_module_name)
+        write_tests_to_file(fp, imports, contents)
