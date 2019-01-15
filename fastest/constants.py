0e8cfd1fea647ef2c732ce0bc5232e5673c9d1f1
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-01-16 02:28:42 +0530

    add: constants for patterns contents and keys

---
+++ fastest/constants.py
@@ -0,0 +1,43 @@
+import platform
+
+
+class SYS:
+    __UNIX_SLASH = '/'
+    __WINDOWS_SLASH = '\\'
+    PY = '.py'
+    SLASH = __WINDOWS_SLASH if platform.system == 'Windows' else __UNIX_SLASH
+    TEST_FILE_ENDING = '__test.py'
+
+
+class KEYS:
+    IMPORTS = 'imports'
+    NAME = 'name'
+    TESTS = 'tests'
+    TEST = 'test'
+    STR = 'str'
+    EXAMPLES = 'examples'
+    FROM = 'from'
+    EXPECT = 'expect'
+    VARIABLES = 'variables'
+
+
+class CONTENT:
+    CLASS_CREATE_TEMPLATE = '\nclass Test{}{}(unittest.TestCase):\n'
+    IMPORT_UNITTEST = 'import unittest\n'
+    DEPS_IMPORT_TEMPLATE = 'from {} import {}\n'
+    TEST_CASE_TEMPLATE = '\tdef test__{function_name}__{case_id}(self):\n'
+    TESTERS_NOTES_TEMPLATE = '\t\t{testers_notes}\n\n'
+    VARIABLES_TEMPLATE = '\t\t{variables}\n'
+    ASSERTION_TEMPLATE = '\t\tself.assertEqual({function}, {value})\n\n'
+
+
+class PATTERNS:
+    FUNCTION_CALL = r'example: [\s\S]+?(?=->)'
+    IMPORT_DEC = '@need\n'
+    VAR_DEC = r'@let '
+    NEED_IMPORT = r'@need[\s\S]+?(?=\n\n)'
+    NEEDED_VARIABLES = r'@let[\s\S]+?(?=\n\n)'
+    NUMBER_BULLET = r'\d\) '
+    TEST_CASE_EXAMPLE = r'\d\) [\s\S]+?(?=\n)'
+    EXAMPLE_PASSAGE = r'-{3,}[\s\S]+?(?=---)'
+    TEST_SEP = ' -> '
