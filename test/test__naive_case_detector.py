75d15a2b5dfa12c076858aabc74f2a13de57ac6d
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-01-28 17:35:39 +0530

    test: add more cases

--- test/test__naive_case_detector.py
+++ test/test__naive_case_detector.py
@@ -11,85 +11,87 @@ import unittest
 
 
 class TestNaiveCaseDetectorFormatImports(unittest.TestCase):
-    def test__format_imports__9EE38D0EC1(self):
-        input_imports = TestBodies.TEST_STACK_IMPORTS_INPUT
+    def test__format_imports__2D5BFFE1F4(self):
+        self.assertIsInstance(format_imports([]), list)
+        input_param = TestBodies.TEST_STACK_IMPORTS_INPUT
         output = TestBodies.TEST_STACK_IMPORTS_OUTPUT
 
-        self.assertEqual(format_imports(input_imports), output)
+        self.assertEqual(format_imports(input_param), output)
 
 
 class TestNaiveCaseDetectorGetImportsFromDocstring(unittest.TestCase):
-    def test__get_imports_from_docstring__0A97C6C157(self):
+    def test__get_imports_from_docstring__B815EEFBE4(self):
+        self.assertIsInstance(get_imports_from_docstring(''), list)
         example_passage = TestBodies.EXAMPLE_WITH_IMPORTS
         import_statements = TestBodies.TEST_IMPORT_EXTRACTION
-
         self.assertEqual(get_imports_from_docstring(example_passage), import_statements)
 
-    def test__get_imports_from_docstring__6714B5906D(self):
+    def test__get_imports_from_docstring__5A0D9E6BC7(self):
+        self.assertIsInstance(get_imports_from_docstring(''), list)
         empty_example_passage = ''
-
         self.assertEqual(get_imports_from_docstring(empty_example_passage), [])
 
 
 class TestNaiveCaseDetectorGetVariablesFromDocstring(unittest.TestCase):
-    def test__get_variables_from_docstring__AEB1E37830(self):
+    def test__get_variables_from_docstring__047B97CCC8(self):
         empty_example_passage = ''
-
         self.assertEqual(get_variables_from_docstring(empty_example_passage), '')
 
-    def test__get_variables_from_docstring__B3CB6ACE59(self):
+    def test__get_variables_from_docstring__34138EF052(self):
         example_passage = TestBodies.TEST_VARIABLES_FROM_DOCSTRING
         expected_output = TestBodies.TEST_VARIABLES_FROM_DOCSTRING_RESULT
-
         self.assertEqual(get_variables_from_docstring(example_passage), expected_output)
 
 
 class TestNaiveCaseDetectorStackExamples(unittest.TestCase):
-    def test__stack_examples__E99E139CF4(self):
+    def test__stack_examples__2BFFE43AC4(self):
+        self.assertIsInstance(stack_examples([]), list)
         self.assertEqual(stack_examples(''), [])
 
-    def test__stack_examples__77FB0AEFA4(self):
+    def test__stack_examples__2CDFB7C509(self):
+        self.assertIsInstance(stack_examples([]), list)
         example_strings = TestBodies.STACK_EXAMPLES_TEST
         self.assertEqual(stack_examples(example_strings), [{'expect': '25', 'from': 'square(5)'}])
 
+    def test__stack_examples__1957DF3889(self):
+        self.assertIsInstance(stack_examples([]), list)
+        self.assertEqual(stack_examples(['1) func_do_work()']), [])
+
 
 class TestNaiveCaseDetectorGetParamsFromDocstring(unittest.TestCase):
-    def test__get_params_from_docstring__75FCBBAE6C(self):
+    def test__get_params_from_docstring__1DD2A843F8(self):
+        self.assertIsInstance(get_params_from_docstring(''), list)
         self.assertEqual(get_params_from_docstring(''), [])
 
-    def test__get_params_from_docstring__D01A5D4D7A(self):
+    def test__get_params_from_docstring__EB16B9484B(self):
+        self.assertIsInstance(get_params_from_docstring(''), list)
         statements = TestBodies.GET_PARAMS_FROM_DOCSTRING_TEST
-
         self.assertEqual(get_params_from_docstring(statements), TestBodies.EXPECT_PARAMS)
 
 
 class TestNaiveCaseDetectorGetReturnFromDocstring(unittest.TestCase):
-    def test__get_return_from_docstring__C55EDAEA80(self):
+    def test__get_return_from_docstring__94E0B4C626(self):
         self.assertEqual(get_return_from_docstring(''), None)
 
-    def test__get_return_from_docstring__3AFD7DDF98(self):
+    def test__get_return_from_docstring__AC31949F08(self):
         statements = TestBodies.RETURN_TYPE_TEST
-
         self.assertEqual(get_return_from_docstring(statements), 'int')
 
 
 class TestNaiveCaseDetectorGetTestCaseExamples(unittest.TestCase):
-    def test__get_test_case_examples__B12AC7941B(self):
+    def test__get_test_case_examples__FE5AB1B2BD(self):
+        self.assertIsInstance(get_test_case_examples(''), list)
         example_passage = TestBodies.TEST_EXAMPLE_PASSAGE
-
         self.assertEqual(get_test_case_examples(example_passage), TestBodies.TEST_EXAMPLE_PASSAGE_RESULT)
 
 
 class TestNaiveCaseDetectorGetTestFromExamplePassage(unittest.TestCase):
-    def test__get_test_from_example_passage__786D0BA0F1(self):
+    def test__get_test_from_example_passage__32197FA47F(self):
         statements = TestBodies.NAIVE_CASE_TEST_STATEMENT
-
         self.assertEqual(get_test_from_example_passage(statements), TestBodies.NAIVE_CASE_TEST_RESULT)
 
-    def test__get_test_from_example_passage__D2A0AD99BE(self):
-
+    def test__get_test_from_example_passage__B15FF9130E(self):
         self.assertEqual(get_test_from_example_passage(None), None)
 
-    def test__get_test_from_example_passage__4A60B432CB(self):
-
+    def test__get_test_from_example_passage__CB43622455(self):
         self.assertEqual(get_test_from_example_passage('lorem ipsum'), None)
--- test/test__test_case_template_builder.py
+++ test/test__test_case_template_builder.py
@@ -11,30 +11,30 @@ import unittest
 
 
 class TestTestCaseTemplateBuilderCaseGenerator(unittest.TestCase):
-    def test__case_generator__CF76E7AB8C(self):
+    def test__case_generator__F92E878CAB(self):
         self.assertEqual(case_generator('a55eff11-ed51-ecb37-ccba'), 'A55EFF11ED')
 
 
 class TestTestCaseTemplateBuilderGetEmptyOfType(unittest.TestCase):
-    def test__get_empty_of_type__DE31DA54D2(self):
+    def test__get_empty_of_type__48EB5DD1EE(self):
         self.assertEqual(get_empty_of_type('str'), "''")
 
-    def test__get_empty_of_type__CE52A3A55C(self):
+    def test__get_empty_of_type__70308738F8(self):
         self.assertEqual(get_empty_of_type('???'), None)
 
 
 class TestTestCaseTemplateBuilderGetParamsList(unittest.TestCase):
-    def test__get_params_list__2C376145E4(self):
+    def test__get_params_list__C0B11F08FF(self):
         self.assertIsInstance(get_params_list([]), list)
         self.assertEqual(get_params_list(['str', 'str', 'list', 'dict']), ["''", "''", '[]', '{}'])
 
-    def test__get_params_list__9E561CD93C(self):
+    def test__get_params_list__954C1CFA12(self):
         self.assertIsInstance(get_params_list([]), list)
         self.assertEqual(get_params_list(['str', '???']), ["''"])
 
 
 class TestTestCaseTemplateBuilderCreateTypeTestCase(unittest.TestCase):
-    def test__create_type_test_case__42EC4F72FF(self):
+    def test__create_type_test_case__063F9F8C0F(self):
         self.assertIsInstance(create_type_test_case({}, []), str)
         function_object = {
             'tests': {
@@ -43,12 +43,11 @@ class TestTestCaseTemplateBuilderCreateTypeTestCase(unittest.TestCase):
             'name': 'function_1'
         }
         params = ['str', 'str']
-
         self.assertEqual(create_type_test_case(function_object, params), TestBodies.TYPE_TEST_CASE_1)
 
 
 class TestTestCaseTemplateBuilderCreateTypeTestCaseIfParams(unittest.TestCase):
-    def test__create_type_test_case_if_params__5FE399FFC0(self):
+    def test__create_type_test_case_if_params__0AB89D903D(self):
         self.assertIsInstance(create_type_test_case_if_params({}, []), str)
         function_object = {
             'tests': {
@@ -56,46 +55,39 @@ class TestTestCaseTemplateBuilderCreateTypeTestCaseIfParams(unittest.TestCase):
             },
             'name': 'function_1'
         }
-
         params = ['str', 'str']
-
         self.assertEqual(create_type_test_case_if_params(function_object, params), TestBodies.TYPE_TEST_CASE_2)
 
 
 class TestTestCaseTemplateBuilderIsTypeTestReady(unittest.TestCase):
-    def test__is_type_test_ready__862FF453D5(self):
+    def test__is_type_test_ready__907046FD09(self):
         self.assertIsInstance(is_type_test_ready({}, []), bool)
         function_object_1 = {
             'tests': {
                 'return': 'str'
             }
         }
-
         params = ['str', 'str']
-
         self.assertEqual(is_type_test_ready(function_object_1, params), True)
 
-    def test__is_type_test_ready__732B88EE66(self):
+    def test__is_type_test_ready__FE3FEEA3FE(self):
         self.assertIsInstance(is_type_test_ready({}, []), bool)
         function_object_2 = {'tests': {}}
-
         params = ['str', 'str']
-
         self.assertEqual(is_type_test_ready(function_object_2, params), False)
 
-    def test__is_type_test_ready__B58D5EA9CF(self):
+    def test__is_type_test_ready__F6B55955F8(self):
         self.assertIsInstance(is_type_test_ready({}, []), bool)
         function_object_1 = {
             'tests': {
                 'return': 'str'
             }
         }
-
         self.assertEqual(is_type_test_ready(function_object_1, []), False)
 
 
 class TestTestCaseTemplateBuilderCreateAssertionTest(unittest.TestCase):
-    def test__create_assertion_test__345956EE83(self):
+    def test__create_assertion_test__69F63013F3(self):
         self.assertIsInstance(create_assertion_test({}), str)
         function_object_1 = {
             'tests': {
@@ -105,15 +97,14 @@ class TestTestCaseTemplateBuilderCreateAssertionTest(unittest.TestCase):
 
         self.assertEqual(create_assertion_test(function_object_1), TestBodies.ASSERTION_TEST_1)
 
-    def test__create_assertion_test__6F2E61E99C(self):
+    def test__create_assertion_test__E42EDE6A4C(self):
         self.assertIsInstance(create_assertion_test({}), str)
         function_object_2 = {'tests': {'variables': []}}
-
         self.assertEqual(create_assertion_test(function_object_2), '')
 
 
 class TestTestCaseTemplateBuilderCreateNaiveTestCase(unittest.TestCase):
-    def test__create_naive_test_case__32E93B1C62(self):
+    def test__create_naive_test_case__46C5250A64(self):
         self.assertIsInstance(create_naive_test_case({}, {}, ''), str)
         function_object = {
             'tests': {
@@ -129,5 +120,4 @@ class TestTestCaseTemplateBuilderCreateNaiveTestCase(unittest.TestCase):
         }
 
         test_id = 'a55eff11-ed51-ecb37-ccba'
-
         self.assertEqual(create_naive_test_case(function_object, test, test_id), TestBodies.NAIVE_TEST_RESULT)
