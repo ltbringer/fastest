14c447807482c7011148813bd6d1294f92d98eb0
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-02-07 23:59:04 +0530

    add: method to create bit strings of given length - assumes length as 8 if not provided

--- fastest/generator/byte_strings.py
+++ fastest/generator/byte_strings.py
@@ -1,3 +1,6 @@
+import random
 import binascii
 
-binascii.unhexlify('%x' % int('0b110100001100101011011000110110001101111', 2)
+
+def gen_string(length=8):
+    return '0b'+''.join([random.choice(['0', '1']) for _ in range(length)])
