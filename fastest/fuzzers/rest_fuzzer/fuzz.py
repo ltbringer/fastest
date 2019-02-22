23c62609e767db729f9c0a8feda82c6a1f8405b9
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-02-22 22:59:28 +0530

    add: mutation - json schema will not be exactly replicated

--- fastest/fuzzers/rest_fuzzer/fuzz.py
+++ fastest/fuzzers/rest_fuzzer/fuzz.py
@@ -1,25 +1,34 @@
+import random
 from fastest.fuzzers.primitive_fuzzer.fuzz import random_integers, random_ascii_chars, random_float
 
 
-def build_empty_of_type(item_type):
+def build_one_of_type(item_type, p=1.0):
     empty_values_for_types = {
         "str": random_ascii_chars,
         "int": random_integers,
         "float": random_float,
         "list": lambda: [],
-        "dict": lambda: {}
+        "dict": lambda: {},
+        "null": lambda: None
     }
-    return empty_values_for_types[item_type]
+    basic_types = ['str', 'int', 'float', 'list', 'dict', 'null']
+    mutation = random.random()
+    return empty_values_for_types[item_type] \
+        if p > mutation \
+        else empty_values_for_types[random.choice(basic_types)]
 
 
-def schema_to_object_builder(schema, name='__root__'):
-    root_object = build_empty_of_type(schema['type'])()
+def schema_to_object_builder(schema, p=1.0):
+    mutation = random.random()
+    root_object = build_one_of_type(schema['type'])()
 
-    if schema['type'] == 'list':
-        root_object.append(schema_to_object_builder(schema['inner']))
+    if isinstance(root_object, list):
+        if p > mutation:
+            root_object.append(schema_to_object_builder(schema['inner'], p=p))
 
-    elif schema['type'] == 'dict':
+    elif isinstance(root_object, dict):
         for prop in schema['inner']:
-            root_object[prop['name']] = schema_to_object_builder(prop)
+            if p > mutation:
+                root_object[prop['name']] = schema_to_object_builder(prop, p=p)
 
     return root_object
