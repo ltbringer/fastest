3f6c1d31c1d7d0a08081f5afb3b8ec14ce8548d2
AmreshVenugopal <amresh.venugopal@gmail.com>
2019-02-22 22:30:17 +0530

    add: tests for json fuzzer

--- test/test__json_schema.py
+++ test/test__json_schema.py
@@ -1,54 +1,59 @@
-from fastest.rest_fuzzer.json_schema import get_schema_for
-from fastest.rest_fuzzer.json_schema import get_schema_for_dict
-from fastest.rest_fuzzer.json_schema import get_schema_for_list
-from fastest.rest_fuzzer.json_schema import get_schema_for_primitive
-from fastest.rest_fuzzer.json_schema import make_schema_object
-from fastest.rest_fuzzer.json_schema import schema_fn
-from fastest.rest_fuzzer.json_schema import type_name
+from fastest.fuzzers.rest_fuzzer.json_schema import get_schema_for
+from fastest.fuzzers.rest_fuzzer.json_schema import get_schema_for_dict
+from fastest.fuzzers.rest_fuzzer.json_schema import get_schema_for_list
+from fastest.fuzzers.rest_fuzzer.json_schema import get_schema_for_primitive
+from fastest.fuzzers.rest_fuzzer.json_schema import make_schema_object
+from fastest.fuzzers.rest_fuzzer.json_schema import schema_fn
+from fastest.fuzzers.rest_fuzzer.json_schema import type_name
 import unittest
 
+
 class TestJsonSchemaTypeName(unittest.TestCase):
-    def test__type_name__5313FC4E00(self):
+    def test__type_name__321A000F63(self):
         self.assertEqual(type_name('apple'), 'str')
 
+
 class TestJsonSchemaSchemaFn(unittest.TestCase):
-    def test__schema_fn__77FB22598D(self):
+    def test__schema_fn__76ED5A360F(self):
         self.assertEqual(schema_fn("str"), get_schema_for_primitive)
 
+
 class TestJsonSchemaGetSchemaForPrimitive(unittest.TestCase):
-    def test__get_schema_for_primitive__6A3023DB00(self):
-        self.assertEqual(get_schema_for_primitive('', ''), None)
+    def test__get_schema_for_primitive__A070E8DF88(self):
+        self.assertEqual(get_schema_for_primitive('', ''), [])
+
 
 class TestJsonSchemaGetSchemaForList(unittest.TestCase):
-    def test__get_schema_for_list__B0C646A2CF(self):        
+    def test__get_schema_for_list__76EB9EE50E(self):
         schema_object = {
             'inner': [{
-                'inner': None,
+                'inner': [],
                 'name': 'key',
                 'type': 'int'
             }],
             'name': '__auto__',
             'type': 'dict'
         }
-        
 
         self.assertEqual(get_schema_for_list([{"key": 1}], "list"), schema_object)
 
+
 class TestJsonSchemaGetSchemaForDict(unittest.TestCase):
-    def test__get_schema_for_dict__5F2E8720CA(self):
-        self.assertEqual(get_schema_for_dict({'key': 1}, 'dict'), [{'type': 'int', 'inner': None, 'name': 'key'}])
+    def test__get_schema_for_dict__7F4D19694B(self):
+        self.assertEqual(get_schema_for_dict({'key': 1}, 'dict'), [{'type': 'int', 'inner': [], 'name': 'key'}])
+
 
 class TestJsonSchemaGetSchemaFor(unittest.TestCase):
-    def test__get_schema_for__B2B60F58A8(self):
-        self.assertEqual(get_schema_for({"key": 1}, 'dict'), [{'type': 'int', 'inner': None, 'name': 'key'}])
+    def test__get_schema_for__E131A99969(self):
+        self.assertEqual(get_schema_for({"key": 1}, 'dict'), [{'type': 'int', 'inner': [], 'name': 'key'}])
+
 
 class TestJsonSchemaMakeSchemaObject(unittest.TestCase):
-    def test__make_schema_object__0F14007CF7(self):        
+    def test__make_schema_object__C0A5822480(self):
         schema_object = {
-            'inner': [{'inner': None, 'name': 'key', 'type': 'int'}],
+            'inner': [{'inner': [], 'name': 'key', 'type': 'int'}],
             'name': '__root__',
             'type': 'dict'
         }
-        
 
         self.assertEqual(make_schema_object({"key": 1}), schema_object)
